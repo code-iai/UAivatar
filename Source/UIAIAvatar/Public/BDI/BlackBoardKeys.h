6777658ca9b479aef9be102d770628d26f7680fd
Mona Abdel-Keream <abdelker@uni-bremen.de>
2022-02-23 17:53:40 +0100

    ai tags and bb keys

---
+++ Source/UIAIAvatar/Public/BDI/BlackBoardKeys.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "Runtime/Core/Public/UObject/NameTypes.h"
+#include "Runtime/Core/Public/Containers/UnrealString.h"
+
+namespace BBKeys
+{
+	/*TCHAR const* const target_location = TEXT("TargetLocation");
+	TCHAR const* const can_see_player = TEXT("CanSeePlayer");
+	TCHAR const* const patrol_path_vector = TEXT("PatrolPathVector");
+	TCHAR const* const patrol_path_index = TEXT("PatrolPathIndex");
+	TCHAR const* const player_is_in_melee_range = TEXT("PlayerIsInMeleeRange");
+	TCHAR const* const is_investigating = TEXT("IsInvestigating");*/
+
+	TCHAR const* const shelf_is_empty = TEXT("ShelfIsEmpty");
+}
---
+++ Source/UIAIAvatar/Public/BDI/ai_tags.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "Runtime/Core/Public/UObject/NameTypes.h"
+#include "Runtime/Core/Public/Containers/UnrealString.h"
+
+namespace tags
+{
+	TCHAR const* const EmptyShelfTag = TEXT("EmptyShelf");
+}
